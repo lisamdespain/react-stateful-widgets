5a0d7fe671875d66de6fff7ff88d48ca1912670e
Lisa DeSpain <lisamdespain@gmail.com>
2022-04-18 14:31:04 -0500

    working up through the spinner project

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -46,37 +46,40 @@ STEP 6:
   This click handler needs to use 'setCount' to set the 'count' to be zero again.
 */
 
-import React from 'react'; /* STEP 0 */
+import React, { useState } from 'react'; /* STEP 0 */
 
 export default function Counter() {
   /* STEP 1 */
+  const [count, setCount] = useState(0);
 
-  const increment = () => {
+  const increment = (num) => {
     /* STEP 4 */
+    setCount(count + num);
   };
-  const decrement = () => {
+  const decrement = (num) => {
     /* STEP 5 */
+    setCount(count - num);
   };
-  const reset = () => {
-    /* STEP 6 */
+  const reset = (num) => {
+    setCount(num);
   };
 
   const style = {
     fontSize: '1.5em',
     marginBottom: '0.3em',
-    color: 'royalblue', /* STEP 2 */
+    color: count % 2 === 0 ? 'royalblue' : 'crimson' /* STEP 2 */
   };
 
   return (
     <div className='widget-counter container'>
       <h2>Counter</h2>
       <div id='count' style={style}>
-        Number 0 is even {/* STEP 3 */}
+        Number {count} is {count % 2 === 0 ? "even" : "odd"} {/* STEP 3 */}
       </div>
       <div>
-        <button id='increment' onClick={increment}>Increment</button>
-        <button id='decrement' onClick={decrement}>Decrement</button>
-        <button id='resetCount' onClick={reset}>Reset</button>
+        <button id='increment' onClick={() => increment(1)}>Increment</button>
+        <button id='decrement' onClick={() => decrement(1)}>Decrement</button>
+        <button id='resetCount' onClick={() => reset(0)}>Reset</button>
       </div>
     </div>
   );
